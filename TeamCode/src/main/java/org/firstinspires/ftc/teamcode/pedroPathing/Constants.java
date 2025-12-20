8313468b5d47e5b0bb0dbb440c6e5ad6be04b950
chiem <chiemwhuama@gmail.com>
2025-12-20 10:15:13 -1000

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -16,10 +16,10 @@ public class Constants {
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1.0d)
-            .rightFrontMotorName("rightFront")
-            .leftFrontMotorName("leftFront")
-            .rightRearMotorName("rightBack")
-            .leftRearMotorName("leftBack")
+            .rightFrontMotorName("rightFront") // The name we gave to the right front mecanum motor in the Robot Configuration
+            .leftFrontMotorName("leftFront") // The name we gave to the left front mecanum motor in the Robot Configuration
+            .rightRearMotorName("rightBack")  // The name we gave to the right rear mecanum motor in the Robot Configuration
+            .leftRearMotorName("leftBack")  // The name we gave to the left rear mecanum motor in the Robot Configuration
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
