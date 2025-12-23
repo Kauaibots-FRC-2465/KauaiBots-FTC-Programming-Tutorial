84290fedd5c0923b7ec6e6f2c0242d9564eb9916
chiem <chiemwhuama@gmail.com>
2025-12-22 19:34:27 -1000

    Remove secondary drive, update auto values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -18,7 +18,7 @@ public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(9.65) // Added per https://pedropathing.com/docs/pathing/tuning/setup "Setting your robot's mass"
             .forwardZeroPowerAcceleration(-29.0d) // Added per https://pedropathing.com/docs/pathing/tuning/automatic "Forward Zero Power Acceleration"
-            .lateralZeroPowerAcceleration(-50.0d)  // Added per https://pedropathing.com/docs/pathing/tuning/automatic "Lateral Zero Power Acceleration"
+            .lateralZeroPowerAcceleration(-60.0d)  // Added per https://pedropathing.com/docs/pathing/tuning/automatic "Lateral Zero Power Acceleration"
             .useSecondaryTranslationalPIDF(true) // Added per https://pedropathing.com/docs/pathing/tuning/pids *Dual PID System*
             /*far   */.translationalPIDFCoefficients(new PIDFCoefficients(0.05d, 0.0d, 0.006d, 0.11d))  // Added per https://pedropathing.com/docs/pathing/tuning/pids/translational *Update Tuned values Into Your Code*
             /*near  */.secondaryTranslationalPIDFCoefficients(new PIDFCoefficients(0d, 0.0d, 0d, 0d))  // Added per https://pedropathing.com/docs/pathing/tuning/pids/translational *Update Tuned values Into Your Code*
@@ -41,8 +41,8 @@ public class Constants {
             .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .xVelocity(64.3d) // Added per https://pedropathing.com/docs/pathing/tuning/automatic "Forward Velocity Tuner"
-            .yVelocity(52.3d);  // Added per https://pedropathing.com/docs/pathing/tuning/automatic "Lateral Velocity Tuner"
+            .xVelocity(60.0d) // Added per https://pedropathing.com/docs/pathing/tuning/automatic "Forward Velocity Tuner"
+            .yVelocity(46.0d);  // Added per https://pedropathing.com/docs/pathing/tuning/automatic "Lateral Velocity Tuner"
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
             .forwardPodY(130.0d) // The forward/backward (center of left rail) pod is 130mm left of center
