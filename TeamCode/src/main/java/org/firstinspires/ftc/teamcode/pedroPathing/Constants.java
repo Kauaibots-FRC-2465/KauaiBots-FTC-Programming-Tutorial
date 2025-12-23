b62875d6ed146582c8c7783419287a6d60497bf5
chiem <chiemwhuama@gmail.com>
2025-12-22 19:31:51 -1000

    Remove secondary drive, update auto values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -17,7 +17,7 @@ public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(9.65) // Added per https://pedropathing.com/docs/pathing/tuning/setup "Setting your robot's mass"
             .forwardZeroPowerAcceleration(-29.0d) // Added per https://pedropathing.com/docs/pathing/tuning/automatic "Forward Zero Power Acceleration"
-            .lateralZeroPowerAcceleration(-60.0d) // Added per https://pedropathing.com/docs/pathing/tuning/automatic "Lateral Zero Power Acceleration"
+            .lateralZeroPowerAcceleration(-60.0d)  // Added per https://pedropathing.com/docs/pathing/tuning/automatic "Lateral Zero Power Acceleration"
             .useSecondaryTranslationalPIDF(true) // Added per https://pedropathing.com/docs/pathing/tuning/pids *Dual PID System*
             /*far   */.translationalPIDFCoefficients(new PIDFCoefficients(0.05d, 0.0d, 0.006d, 0.11d))  // Added per https://pedropathing.com/docs/pathing/tuning/pids/translational *Update Tuned values Into Your Code*
             /*near  */.secondaryTranslationalPIDFCoefficients(new PIDFCoefficients(0d, 0.0d, 0d, 0d))  // Added per https://pedropathing.com/docs/pathing/tuning/pids/translational *Update Tuned values Into Your Code*
