c7cd9530346a2baaf44813d64d7e672cd4507907
chiem <chiemwhuama@gmail.com>
2025-12-24 08:35:47 -1000

    Add a feature: Recorient the robot when you press triangle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTeleop.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.button.GamepadButton;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.subsystems.PedroPathingSubsystem;
@@ -8,10 +11,14 @@ import org.firstinspires.ftc.teamcode.subsystems.PedroPathingSubsystem;
 @TeleOp(name = "2025 Decode Teleop", group = "20311")
 public class DecodeTeleop extends CommandOpMode {
     PedroPathingSubsystem pps;
+        private GamepadEx driverGamepad;
+        private GamepadButton reorientButton ;
 
     @Override
     public void initialize() {
         pps = new PedroPathingSubsystem(hardwareMap);
+        driverGamepad = new GamepadEx(gamepad1);
+        reorientButton = new GamepadButton(driverGamepad, GamepadKeys.Button.Y);
         pps.cmdSetFieldForwardDirection(0).schedule();
 
         pps.setDefaultCommand(
@@ -22,6 +29,6 @@ public class DecodeTeleop extends CommandOpMode {
                         true
                 )
         );
+        reorientButton.whenPressed(pps.cmdSetFieldForwardDirection());
     }
-
 }
