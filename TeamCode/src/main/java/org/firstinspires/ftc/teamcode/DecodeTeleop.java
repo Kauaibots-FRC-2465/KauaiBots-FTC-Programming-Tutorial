f54cbef491a11a1c3fe3b56559e137573246b22c
chiem <chiemwhuama@gmail.com>
2025-12-23 23:33:27 -1000

    Forgot the @TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTeleop.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.subsystems.PedroPathingSubsystem;
 
+@TeleOp(name = "FTC Decode Teleop", group = "20311")
 public class DecodeTeleop extends CommandOpMode {
     PedroPathingSubsystem pps;
 
