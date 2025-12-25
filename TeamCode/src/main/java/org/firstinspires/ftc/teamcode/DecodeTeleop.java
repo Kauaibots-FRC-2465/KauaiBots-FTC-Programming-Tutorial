b5183059bb75cd89ace0cea6d4e03d2bc97e8da4
chiem <chiemwhuama@gmail.com>
2025-12-24 15:56:33 -1000

    fix minor mismatches and cherry pick changes from prior steps

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTeleop.java
@@ -2,33 +2,22 @@ package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.command.Command;
 import com.arcrobotics.ftclib.command.CommandOpMode;
-import com.arcrobotics.ftclib.command.button.GamepadButton;
-import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 import org.firstinspires.ftc.teamcode.subsystems.PedroPathingSubsystem;
-
 import java.util.function.Supplier;
 
 @TeleOp(name = "2025 Decode TeleOp", group = "20311")
 public class DecodeTeleop extends CommandOpMode {
     private PedroPathingSubsystem pps;
-    private GamepadEx driverGamepad;
-    private GamepadButton reorientButton;
     @Override
     public void initialize() {
         pps = new PedroPathingSubsystem(hardwareMap);
-        driverGamepad = new GamepadEx(gamepad1);
-        reorientButton = new GamepadButton(driverGamepad, GamepadKeys.Button.Y);
         pps.cmdSetFieldForwardDirection(0).schedule();
         Supplier<Float> fwdSupplier = () -> -gamepad1.left_stick_y;
         Supplier<Float> strafeSupplier = () -> -gamepad1.left_stick_x;
         Supplier<Float> turnSupplier = () -> -gamepad1.right_stick_x;
         Command goFieldOriented = pps.cmdGoFieldOriented(fwdSupplier, strafeSupplier, turnSupplier, true);
-        Command reorient=pps.cmdSetFieldForwardDirection();
 
         pps.setDefaultCommand(goFieldOriented);
-        reorientButton.whenPressed(reorient);
     }
 }
