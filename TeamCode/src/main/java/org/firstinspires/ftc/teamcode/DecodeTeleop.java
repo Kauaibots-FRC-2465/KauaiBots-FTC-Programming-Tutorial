a52eb3b65be613287410daf2c9aacaaf2915fb89
chiem <chiemwhuama@gmail.com>
2025-12-24 16:19:00 -1000

    Bring changes from prior steps forward, later steps backward

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTeleop.java
@@ -1,26 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.arcrobotics.ftclib.command.Command;
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import org.firstinspires.ftc.teamcode.subsystems.PedroPathingSubsystem;
+
+import java.util.function.Supplier;
 
 @TeleOp(name = "2025 Decode TeleOp", group = "20311")
 public class DecodeTeleop extends CommandOpMode {
     private PedroPathingSubsystem pps;
-
     @Override
     public void initialize() {
         pps = new PedroPathingSubsystem(hardwareMap);
+        Supplier<Float> fwdSupplier = () -> -gamepad1.left_stick_y;
+        Supplier<Float> strafeSupplier = () -> -gamepad1.left_stick_x;
+        Supplier<Float> turnSupplier = () -> -gamepad1.right_stick_x;
+        Command goFieldOriented = pps.cmdGoFieldOriented(fwdSupplier, strafeSupplier, turnSupplier, true);
+
         pps.cmdSetFieldForwardDirection(0).schedule();
-
-        pps.setDefaultCommand(
-                pps.cmdGoFieldOriented(
-                        () -> -gamepad1.left_stick_y,
-                        () -> -gamepad1.left_stick_x,
-                        () -> -gamepad1.right_stick_x,
-                        true
-                )
-        );
+        pps.setDefaultCommand(goFieldOriented);
     }
-
 }
