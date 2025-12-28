7ccb2b757592a5fa76adbd621ecf0e620d5c2821
chiem <chiemwhuama@gmail.com>
2025-12-27 21:25:17 -1000

    cant complie for some reason

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FlywheelSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FlywheelSubsystem.java
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
@@ -11,8 +13,13 @@ import com.seattlesolvers.solverslib.controller.PIDController;
 import java.util.ArrayList;
 
 public class FlywheelSubsystem extends SubsystemBase {
+    // hardware references
     private HardwareMap hardwareMap;
     private VoltageSensor controlHubVSensor;
+    private ArrayList<DcMotorEx> flywheelMotors = new ArrayList<>();
+    private DcMotorEx encoderMotor = null;
+
+    // flywheel data
     private double countsPerFlywheelRotation;
     private double flywheelDiameterInches;
 
@@ -28,5 +35,21 @@ public class FlywheelSubsystem extends SubsystemBase {
                 " flywheelDiameterInches cannot be 0.");
         if (countsPerFlywheelRotation == 0) throw new IllegalArgumentException ("ASSERTION FAILED:"+
                 " countsPerFlywheelRotation cannot be 0.");
+    }
+
+    public void addFlywheelMotor(String motorName, DcMotorSimple.Direction direction) {
+        try {
+            DcMotorEx motor = hardwareMap.get(DcMotorEx.class, motorName);
+            motor.setDirection(direction);
+            motor.setPower(0);
+            motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            if (encoderMotor == null) encoderMotor = motor;
+            flywheelMotors.add(motor);
+        }
+        catch (Exception ignored) {
+            throw new IllegalArgumentException("Failed to get motor " + motorName +".  You may" +
+                    " need to power cycle the Robot.    (Did you verify the name you gave matches" +
+                    " what you set in the robot configuration?)");
+        }
     }
 }
