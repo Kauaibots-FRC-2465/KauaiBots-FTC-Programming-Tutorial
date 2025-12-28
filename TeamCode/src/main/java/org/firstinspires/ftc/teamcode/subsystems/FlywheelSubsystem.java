35da92b64d07d211c0135e428f678fe25e98da7c
chiem <chiemwhuama@gmail.com>
2025-12-27 19:37:29 -1000

    Add constructor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FlywheelSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FlywheelSubsystem.java
@@ -2,10 +2,31 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
+import com.seattlesolvers.solverslib.controller.PIDController;
+
+import java.util.ArrayList;
 
 public class FlywheelSubsystem extends SubsystemBase {
-    private TelemetryManager panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
     private HardwareMap hardwareMap;
+    private VoltageSensor controlHubVSensor;
+    private double countsPerFlywheelRotation;
+    private double flywheelDiameterInches;
+
+    public FlywheelSubsystem(HardwareMap hardwareMap,
+                             VoltageSensor controlHubVSensor,
+                             double countsPerFlywheelRotation,
+                             double flywheelDiameterInches) {
+        this.hardwareMap = hardwareMap;
+        this.controlHubVSensor = controlHubVSensor;
+        this.countsPerFlywheelRotation = countsPerFlywheelRotation;
+        this.flywheelDiameterInches = flywheelDiameterInches;
+        if (flywheelDiameterInches == 0) throw new IllegalArgumentException ("ASSERTION FAILED:"+
+                " flywheelDiameterInches cannot be 0.");
+        if (countsPerFlywheelRotation == 0) throw new IllegalArgumentException ("ASSERTION FAILED:"+
+                " countsPerFlywheelRotation cannot be 0.");
+    }
 }
