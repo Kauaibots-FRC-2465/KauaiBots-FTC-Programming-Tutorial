b518ac90fa281e362613d838453ac5b5afdfbc7f
chiem <chiemwhuama@gmail.com>
2025-12-28 21:19:10 -1000

    Need to initialize motorVoltageSupplier

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FlywheelSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FlywheelSubsystem.java
@@ -27,7 +27,7 @@ public class FlywheelSubsystem extends SubsystemBase {
     private double[] voltageHistory = {12d, 12d, 12d, 12d, 12d, 12d, 12d, 12d};
     private double batteryVoltage = 12d;
     private final DoubleSupplier idle = () -> 0;
-    private DoubleSupplier motorVoltageSupplier; // Commands must provide their own supplier
+    private DoubleSupplier motorVoltageSupplier = idle; // Commands must provide their own supplier
     private double motorVoltage;
     private double kS = 0;
 
