513a12d6555359485f9d5a510aa5f5f7b1818e61
chiem <chiemwhuama@gmail.com>
2025-12-28 20:41:22 -1000

    Make it simpler, don't bother to get expansion hub voltage sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTuning.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
 import com.seattlesolvers.solverslib.command.button.GamepadButton;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
@@ -10,11 +11,20 @@ import org.firstinspires.ftc.teamcode.subsystems.FlywheelSubsystem;
 
 @TeleOp(name = "2025 Decode Tuning", group = "20311")
 public class DecodeTuning extends CommandOpMode {
+    private VoltageSensor controlHubVSensor = null;
+    private FlywheelSubsystem fs;
     private GamepadEx driverGamepad;
     private GamepadButton flywheelTuneMotorConstantsButton, flywheelIdleButton;
-    private FlywheelSubsystem fs;
     @Override
     public void initialize() {
+        try {
+            controlHubVSensor = hardwareMap.get(VoltageSensor.class, "Control Hub");
+        }
+        catch (Exception ignored) {
+            throw new IllegalArgumentException("Failed to get Control Hub voltage sensor."+
+                    "  You may need to power cycle the Robot.");
+        }
+        fs = new FlywheelSubsystem(hardwareMap, controlHubVSensor, 28, 3);
         driverGamepad = new GamepadEx(gamepad1);
         flywheelTuneMotorConstantsButton = new GamepadButton(driverGamepad, GamepadKeys.Button.A);
         flywheelIdleButton = new GamepadButton(driverGamepad, GamepadKeys.Button.B);
