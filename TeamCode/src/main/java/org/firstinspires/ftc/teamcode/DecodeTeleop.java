e322f988b01429c7939fe530376fa08d9cf5659d
chiem <chiemwhuama@gmail.com>
2025-12-29 14:25:11 -1000

    Cross-sync

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTeleop.java
@@ -14,13 +14,14 @@ import java.util.function.Supplier;
 @TeleOp(name = "2025 Decode TeleOp", group = "20311")
 public class DecodeTeleop extends CommandOpMode {
     private PedroPathingSubsystem pps;
-    private GamepadEx driverGamepad;
+    private GamepadEx driverGamepad, engineerGamepad;
     private GamepadButton reorientButton;
 
     @Override
     public void initialize() {
         pps = new PedroPathingSubsystem(hardwareMap);
         driverGamepad = new GamepadEx(gamepad1);
+        engineerGamepad = new GamepadEx(gamepad2);
         reorientButton = new GamepadButton(driverGamepad, GamepadKeys.Button.Y);
         Supplier<Float> fwdSupplier = () -> -gamepad1.left_stick_y;
         Supplier<Float> strafeSupplier = () -> -gamepad1.left_stick_x;
