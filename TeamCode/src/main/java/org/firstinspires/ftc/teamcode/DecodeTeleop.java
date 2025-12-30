f9dd4032bd7ffa47d0b3b2b2535e51ccb8e284a8
chiem <chiemwhuama@gmail.com>
2025-12-29 21:04:28 -1000

    Make match instructions and propagate errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTeleop.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.seattlesolvers.solverslib.command.Command;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
 import com.seattlesolvers.solverslib.command.button.GamepadButton;
@@ -10,7 +8,6 @@ import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
 import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.subsystems.FlywheelSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.PedroPathingSubsystem;
 
 import java.util.function.Supplier;
@@ -18,24 +15,12 @@ import java.util.function.Supplier;
 @TeleOp(name = "2025 Decode TeleOp", group = "20311")
 public class DecodeTeleop extends CommandOpMode {
     private PedroPathingSubsystem pps;
-    private FlywheelSubsystem fs;
-    private VoltageSensor controlHubVSensor = null;
     private GamepadEx driverGamepad, engineerGamepad;
     private GamepadButton reorientButton, driverCentricButton;
 
     @Override
     public void initialize() {
-        try {
-            controlHubVSensor = hardwareMap.get(VoltageSensor.class, "Control Hub");
-        }
-        catch (Exception ignored) {
-            throw new IllegalArgumentException("Failed to get Control Hub voltage sensor."+
-                    "  You may need to power cycle the Robot.");
-        }
         pps = new PedroPathingSubsystem(hardwareMap, new Pose(0+17.2/2, 24, 0));
-        fs = new FlywheelSubsystem(hardwareMap, controlHubVSensor, 28, 3);
-        fs.addFlywheelMotor("shooter1", DcMotorSimple.Direction.REVERSE);
-        fs.addFlywheelMotor("shooter2", DcMotorSimple.Direction.REVERSE);
         driverGamepad = new GamepadEx(gamepad1);
         engineerGamepad = new GamepadEx(gamepad2);
         reorientButton = new GamepadButton(driverGamepad, GamepadKeys.Button.TRIANGLE); // aka Y
