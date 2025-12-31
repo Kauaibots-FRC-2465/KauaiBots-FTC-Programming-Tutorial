eab8ae9caf59c07f57c5aab05706877016c234f8
chiem <chiemwhuama@gmail.com>
2025-12-30 17:13:24 -1000

    Add cmdUnjam to DecodeTeleop and propagate changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTuning.java
@@ -1,8 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
+import com.seattlesolvers.solverslib.command.Command;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
+import com.seattlesolvers.solverslib.command.InstantCommand;
+import com.seattlesolvers.solverslib.command.WaitCommand;
 import com.seattlesolvers.solverslib.command.button.GamepadButton;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
 import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
@@ -15,12 +20,12 @@ public class DecodeTuning extends CommandOpMode {
     private FlywheelSubsystem fs;
     private VoltageSensor controlHubVSensor = null;
     private GamepadEx driverGamepad;
-    private GamepadButton
-            findMotorConstantsButton,
-            idleButton,
-            tuneButton,
-            increasePButton,
-            decreasePButton;
+    private GamepadButton findMotorConstantsButton;
+    private GamepadButton stopButton;
+    private GamepadButton tuneButton;
+    private GamepadButton increasePButton;
+    private GamepadButton decreasePButton;
+    private GamepadButton testLaunchDetectionButton;
     @Override
     public void initialize() {
         try {
@@ -35,14 +40,40 @@ public class DecodeTuning extends CommandOpMode {
         fs.addFlywheelMotor("shooter2", DcMotorSimple.Direction.REVERSE);
         driverGamepad = new GamepadEx(gamepad1);
         findMotorConstantsButton = new GamepadButton(driverGamepad, GamepadKeys.Button.CROSS); // aka A;
-        idleButton = new GamepadButton(driverGamepad, GamepadKeys.Button.CIRCLE); // aka B
+        stopButton = new GamepadButton(driverGamepad, GamepadKeys.Button.CIRCLE); // aka B
         tuneButton = new GamepadButton(driverGamepad, GamepadKeys.Button.SQUARE); // aka X
         increasePButton = new GamepadButton(driverGamepad, GamepadKeys.Button.DPAD_UP);
         decreasePButton = new GamepadButton(driverGamepad, GamepadKeys.Button.DPAD_DOWN);
+        testLaunchDetectionButton = new GamepadButton(driverGamepad, GamepadKeys.Button.LEFT_BUMPER);
         findMotorConstantsButton.whenPressed(fs.cmdFindMotorConstants());
-        idleButton.whenPressed(fs.cmdStop());
+        stopButton.whenPressed(fs.cmdStop());
         tuneButton.whenHeld(fs.cmdTuneWithTelemetry(1500d));
         increasePButton.whenPressed(fs.cmdIncreaseP());
         decreasePButton.whenPressed(fs.cmdDecreaseP());
+        Command launch = fs.cmdSetRPM(()->1500, ()->false);
+        Command detectLaunch =
+            new WaitCommand(50).andThen(
+            fs.cmdWaitUntilStable(),
+            cmdLog("Stable "+System.nanoTime()),
+            fs.cmdWaitLaunchStart(1500, 200),
+            cmdLog("Launch started at "+System.nanoTime()),
+            fs.cmdWaitLaunchEnd(5),
+            cmdLog("Launch ended at "+System.nanoTime()));
+        testLaunchDetectionButton.whenPressed(launch.raceWith(detectLaunch));
+        testLaunchDetectionButton.whenPressed(
+                fs.cmdSetRPM(()->1500, ()->false)
+                    .raceWith(
+                new WaitCommand(50).andThen(
+                            fs.cmdWaitUntilStable(),
+                            cmdLog("Stable "+System.nanoTime()),
+                            fs.cmdWaitLaunchStart(1500, 200),
+                            cmdLog("Launch started at "+System.nanoTime()),
+                            fs.cmdWaitLaunchEnd(5),
+                            cmdLog("Launch ended at "+System.nanoTime()))));
+
+    }
+
+    public Command cmdLog (String info) {
+        return new InstantCommand(()-> Log.i("FTC20311", info));
     }
 }
