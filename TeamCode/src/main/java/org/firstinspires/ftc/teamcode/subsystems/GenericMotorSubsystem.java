3ccafc0b909110117700fe679f9fa2a6755692a1
chiem <chiemwhuama@gmail.com>
2026-01-01 20:43:19 -1000

    values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/GenericMotorSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/GenericMotorSubsystem.java
@@ -1,5 +1,12 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.STOP_AND_RESET_ENCODER;
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
 
 public class GenericMotorSubsystem extends SubsystemBase {
@@ -29,4 +36,38 @@ public class GenericMotorSubsystem extends SubsystemBase {
     }
 
     private double countsPerRotation;
+    private final DcMotorEx motor;
+    // Default values
+    private double positionP = 10d;
+    private double positionPower = 1d;
+    private double velocityP = 0d;
+    private double velocityF = 0d;
+    private DcMotor.ZeroPowerBehavior defaultZPB = BRAKE;
+    // RunMode cache
+    private DcMotor.RunMode lastRunMode;
+    // RUN_USING_ENCODER cache
+    private final DcMotorExCache lastRUEcps  = new DcMotorExCache(20d);
+    // RUN_WITHOUT_ENCODER cache
+    private final DcMotorExCache lastRWEpower = new DcMotorExCache(1d/32767d);
+    // RUN_TO_POSITION cache
+    private final DcMotorExCache lastRTPcounts = new DcMotorExCache(1d);
+    // ZeroPowerBehavior cache
+    private DcMotor.ZeroPowerBehavior lastZPB;
+
+    public GenericMotorSubsystem(HardwareMap hardwareMap, String motorName, DcMotorSimple.Direction direction, double countsPerRotation) {
+        this.countsPerRotation = countsPerRotation;
+        try {
+            motor = hardwareMap.get(DcMotorEx.class, motorName);
+        }
+        catch (Exception ignored) {
+            throw new IllegalArgumentException("Failed to get motor " + motorName + ".  You may" +
+                    " need to power cycle the Robot.    (Did you verify the name you gave matches" +
+                    " what you set in the robot configuration?)");
+        }
+        motor.setDirection(direction);
+        motor.setZeroPowerBehavior(defaultZPB);
+        lastZPB = defaultZPB;
+        motor.setMode(STOP_AND_RESET_ENCODER);
+        lastRunMode = STOP_AND_RESET_ENCODER;
+    }
 }
