8733d4009e3ec59299fa56ddbfae35c16a77cfff
chiem <chiemwhuama@gmail.com>
2026-01-04 00:53:03 -1000

    Fixed encoder direction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -36,7 +36,7 @@ public class Constants {
             .hardwareMapName("odo1") // The name we gave to the odometry computer in the Robot Configuration
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
             .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
-            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);  // Our strafe pod is installed "backward"
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
